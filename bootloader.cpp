a7f9d3126bb2e52b4d7eef1efe8eae46925c60fe
Takumi Sueda <puhitaku@gmail.com>
2022-04-19 01:24:24 +0900

    Remove unused functions and headers

--- bootloader.cpp
+++ bootloader.cpp
@@ -1,47 +1,6 @@
 // This file is in public domain.
 
-#include <algorithm>
-#include <tchar.h>
-#include <vector>
 #include <windows.h>
-
-#define MainWindowClassName L"SelectorMainWindowClass"
-
-#define DataTypeAlertError 0x1000
-#define DataTypeAlertInformation 0x1001
-#define DataTypeAlertWarning 0x1002
-
-static void showAlertWarning(LPCWSTR message, LPCWSTR title)
-{
-
-    HWND selectorMainWindow = FindWindow(MainWindowClassName, NULL);
-
-    if (!selectorMainWindow)
-    {
-        MessageBox(NULL, message, title, MB_ICONWARNING);
-        return;
-    }
-
-    wchar_t data[2048];
-    wcscpy(data, message);
-    data[wcslen(message)] = 0;
-    wcscpy(data + wcslen(message) + 1, title);
-
-    size_t dataLen = wcslen(message) + wcslen(title) + 1;
-
-    COPYDATASTRUCT info;
-    info.dwData = DataTypeAlertWarning;
-    info.cbData = dataLen * sizeof(wchar_t);
-
-    HGLOBAL global = GlobalAlloc(GPTR, info.cbData);
-    memcpy((LPVOID)global, data, info.cbData);
-
-    info.lpData = (LPVOID)global;
-
-    SendMessage(selectorMainWindow, WM_COPYDATA, NULL, (LPARAM)&info);
-
-    GlobalFree(global);
-}
 
 int APIENTRY WinMain(HINSTANCE hInst, HINSTANCE hPrev, LPTSTR lpCmd, int nShow)
 {
